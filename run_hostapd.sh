a85914b543191d7971b396b9fb89d5e8ff9a6139
Gregory N. Schmit <schmitgreg@gmail.com>
2025-10-01 09:56:23 -0500

    Updates to dev env and work on hostapd patch.

--- run_hostapd.sh
+++ run_hostapd.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# First, create symlink for multicall binary with:
-# ln -s ./openwrt/build_dir/target-x86_64_musl/hostapd-wpad-basic-mbedtls/hostapd-2025.08.26~ca266cc2/wpad hostapd
-
 STAGE="$(pwd)/openwrt/staging_dir"
 LIBS="$STAGE/target-x86_64_musl/usr/lib:$STAGE/toolchain-x86_64_gcc-14.3.0_musl/lib"
+PROG="./openwrt/build_dir/target-x86_64_musl/hostapd-full-internal/hostapd-2025.08.26~ca266cc2/hostapd/hostapd"
 
-sudo LD_LIBRARY_PATH="$LIBS:$LD_LIBRARY_PATH" ./hostapd ~/hostapd-test.conf "$@"
+sudo LD_LIBRARY_PATH="$LIBS:$LD_LIBRARY_PATH" $PROG $@ ./hostapd.conf
